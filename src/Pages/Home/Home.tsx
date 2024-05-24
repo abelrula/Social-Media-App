d7136e34c0c38a5c25d33b01b2d8dc81978f6b0c
abel zewdu <abelrula716@gmail.com>
2024-05-24 11:49:41 -0700

    added side notification to home page

--- src/Pages/Home/Home.tsx
+++ src/Pages/Home/Home.tsx
@@ -4,15 +4,17 @@ import { IoIosAttach } from "react-icons/io"
 import { SlPicture } from "react-icons/sl"
 import { BiSearch } from "react-icons/bi"
 import ProfileImage from "../../components/ProfileImage"
+import SideBarAll from "../../components/SideBarAll"
 
  
  const Home = () => {
-   return (
-    <div className="w-[600px] flex flex-col gap-5 overflow-y-scroll overflow-x-hidden scroll-smooth scrollbar-thin">
+    return (
+      <div className="flex gap-8">
+             <div className="w-[550px] flex flex-col gap-5 overflow-y-scroll overflow-x-hidden scroll-smooth scrollbar-thin">
             <div className="bg-white flex flex-row p-2.5">
-            <div className=" flex items-center">
+            <div className="flex items-center">
               <ProfileImage profileName="abel zewdu" />
-                <span className=" bg-slate-100 rounded-xl p-1.5 w-[420px] flex items-center">
+                <span className="bg-slate-100 rounded-xl p-1.5 w-[420px] flex items-center">
                   <BiSearch className='text-zinc-700' /> 
                   <input className="bg-slate-100 h-6 w-full rounded-none outline-none text-xs font-mono text-zinc-700" type='text' placeholder='what`s on your mind ?' />
                 </span>
@@ -23,11 +25,13 @@ import ProfileImage from "../../components/ProfileImage"
                 <IoIosAttach className='icon' />
                 <BsEmojiSmile className='icon' />
               </span> 
-             <button className="rounded-sm px-2.5 cursor-pointer  border-zinc-400 hover:bg-zinc-500 bg-zinc-400">post</button>
+             <button className="rounded-sm px-2.5 cursor-pointer border-zinc-400 hover:bg-zinc-500 bg-zinc-400">post</button>
             </div>
             </div>
             <PostedContents />
-            </div>
+             </div>
+             <SideBarAll />
+      </div>
    )
  }
  
---
+++ src/components/SideBarAll.tsx
@@ -0,0 +1,76 @@
+ import { events, freinds, groups, onlineContacts } from '../data/data'
+import { MdEventSeat } from 'react-icons/md'
+import travel1 from"../assets/travel1.jpg"
+const SideBarAll = () => {
+  return (
+    <>
+        <div className="flex flex-col gap-2.5">
+              <div className="bg-zinc-100 p-2.5 w-56 h-auto rounded-lg overflow-hidden">
+                <h4 className="text-center text-zinc-400 font-mono text-sm">your upcoming events </h4>
+                <ul className="flex flex-col gap-1.5">
+                    {
+                      events.map((event,i)=>(
+                        <li className="flex items-center gap-2.5 no-underline h-8" key={i}><MdEventSeat className='text-green' />
+                        <p className="text-xs font-mono flex flex-col text-zinc-600">{event.event}
+                        <span className='text-zinc-400 font-mono'>{event.time}</span></p></li>
+                      ))
+                    }
+                </ul>
+              </div> 
+              <div className="bg-zinc-100 p-2.5 w-56 h-auto rounded-lg overflow-hidden">
+                <h4 className="text-center text-zinc-400 font-mono text-sm">Birthdays</h4>
+                <ul className="flex flex-col gap-1.5">
+                {
+                  freinds.map((freind,i)=>(
+                    <li className="flex items-center gap-2.5 no-underline h-8" key={i}>
+                        <img className='w-6 h-6 rounded-full object-cover object-center' src={freind.profile} />
+                        <p className="text-xs font-mono flex flex-col text-zinc-600">{freind.name} 
+                        <span className='text-zinc-400 font-mono'>turning 28 years old</span></p></li>
+                  ))
+                 }
+                </ul>
+              </div>
+           </div>
+           <div className="mt--2.5 flex flex-col gap-1.5">
+           <div className="bg-zinc-100 p-2.5 w-56 h-auto rounded-lg overflow-hidden">
+            <h4 className="text-center text-zinc-400 font-mono text-sm">community chats</h4>
+            <ul className="flex flex-col ">
+                 {
+                  events.map((event,i)=>(
+                    <li className="flex items-center gap-2.5 no-underline h-8" key={i}>
+                        <img className='w-6 h-6 rounded-full object-cover object-center' src={travel1} />
+                        <p className="text-xs font-mono flex flex-col text-zinc-600">{event.event}</p></li>
+                  ))
+                 }
+            </ul>
+           </div> 
+           <div className="bg-zinc-100 p-2.5 w-56 h-auto rounded-lg overflow-hidden">
+            <h4 className="text-center text-zinc-400 font-mono text-sm">Group Chats</h4>
+            <ul className="flex flex-col">
+                 {
+                  groups.map((event,i)=>(
+                    <li className="flex items-center gap-2.5 no-underline h-8" key={i}>
+                        <img className='w-6 h-6 rounded-full object-cover object-center' src={event.img} />
+                        <p className="text-xs font-mono flex flex-col text-zinc-600">{event.name} </p></li>
+                  ))
+                 }
+            </ul>
+           </div>
+           <div className="bg-zinc-100 p-2.5 w-56 h-auto rounded-lg overflow-hidden">
+            <h4 className="text-center text-zinc-400 font-mono text-sm">Online Contacts</h4>
+            <ul className="flex flex-col">
+                 {
+                  onlineContacts.map((freind,i)=>(
+                    <li className="flex items-center gap-2.5 no-underline h-8" key={i}>
+                        <img className='w-6 h-6 rounded-full object-cover object-center' src={freind.profile} />
+                        <p className="text-xs font-mono flex flex-col text-zinc-600">{freind.name}</p></li>
+                  ))
+                 }
+            </ul>
+           </div>
+           </div> 
+    </>
+  )
+}
+
+export default SideBarAll
