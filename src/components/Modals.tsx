d214075380aedccd988cb0a0995eb1f1c18785f3
Abel Zewdu <abelrula716@gmail.com>
2024-08-14 04:10:39 -0900

    Implementing sliding image on posted  modal  for more than 2 images & Implementing more than 2 photo snapshot on the side on the of posted content onclick open modal

--- src/components/Modals.tsx
+++ src/components/Modals.tsx
@@ -1,8 +1,15 @@
-import React from 'react'
+import { GrPrevious } from "react-icons/gr";
+import { GrNext } from "react-icons/gr";
+
+import React, { useRef, useState } from 'react'
 import ProfileImage from './ProfileImage'
-import { IoIosClose } from 'react-icons/io'
+import { IoIosClose, IoMdClose } from 'react-icons/io'
 import Comments from './Comments'
 import TextBox from './TextBox'
+import { profileImg } from '../lib/data'
+import  { ReactionsButtons } from './Reactions'
+import 'react-quill/dist/quill.snow.css';
+
  
 type propsTypes = {
   currentModal: postedContentsDataType,
@@ -11,20 +18,44 @@ type propsTypes = {
 }   
 export const PostedContentModal = ({currentModal,setCurrentIndex}:propsTypes) => {
  
+  const [ scrollPosition, setScrollPosition ] = useState(0)
+  const [ imageNums, setImageNums ] = useState(0)
+
+  currentModal.image.length - 1
+  console.log(imageNums);
+  
+    const imageRef = useRef<HTMLImageElement>()
+   
+  const handleNextImage = (imgWidth:number) => {
+    
+    setScrollPosition(scrollPosition + imgWidth)
+    imageRef.current.scrollLeft = imgWidth + scrollPosition
+   
+      // imageNums < currentModal.image.length
+   
+  }
+
   return (
     <div className=" bg-[#000000ad] fixed top-0 bottom-0 flex items-center justify-center left-0 right-0  z-50">
-            <div className="relative bg-white w-[820px] h-[450px] flex gap-4 border rounded-[5px] p-1.5 border-[#d9d9d9] z-50" >
-               <div className="w-11/12 flex h-auto flex-col gap-6 overflow-hidden">
-                 <div className="flexw-full rounded-full gap-0.5 ">
+            <div className="bg-white relative w-[820px] h-[450px] flex gap-4 border rounded-[5px] p-1.5 border-[#d9d9d9] z-50" >
+               <div className="w-11/12 relative flex h-auto flex-col gap-6 overflow-hidden ">
+                  { currentModal.image.length > 1 && imageNums > 0   &&  <GrPrevious onClick={()=>{handleNextImage(-400); setImageNums(prev=>prev-1)}} fontSize={ 25 } className="bg-black  rounded-full p-2 absolute top-[50%] left-0 text-white" />}
+                 <div ref={imageRef} className="flex w-full  gap-0.5 overflow-x-scroll scroll-smooth">
                       {
                         currentModal.image.map((img,i)=>(
-                          <img className='max-h-[430px] w-full object-cover object-center self-center items-end  h-auto' key={i} src={img}  />
+                          <img className='min-h-[500px] min-w-[400px] object-cover rounded-md object-center self-center items-end  h-auto' key={i} src={img}  />
                         ))
                       }
                  </div>
+          { currentModal.image.length > 1  && imageNums < currentModal.image.length - 1  && <GrNext onClick={ () => { handleNextImage(400); setImageNums(prev=>prev+1)} } fontSize={ 25 } className="bg-black  rounded-full p-2 absolute top-[50%] right-0 text-white" /> }
+                <div className="flex absolute bottom-0 gap-2 left-1/2">
+                  { currentModal.image.map((_item, i)=>(
+                    <span key={i} className={`w-2 h-2 rounded-full ${imageNums === i ?"bg-slate-900":"bg-white"}`}></span>
+                  ))}
+                </div>
                </div>
                <div className="w-full flex flex-col gap-4">
-               <IoIosClose fontSize={23} className='text-lg absolute top-0 right-0 hover:text-3xl' onClick={()=>setCurrentIndex(undefined)} />
+               <IoIosClose  fontSize={23} className='text-lg absolute top-0 right-0 hover:text-3xl' onClick={()=>setCurrentIndex(undefined)} />
                    <div className="flex justify-start self-baseline ml-2.5 gap-2.5">
             { currentModal.image ?
               <img className='w-6 h-6 rounded-full object-cover object-center ' src={ currentModal.image[ 0 ] } /> : <ProfileImage profileName={ currentModal.owner } /> }
@@ -36,13 +67,64 @@ export const PostedContentModal = ({currentModal,setCurrentIndex}:propsTypes) =>
             </div>
               </div>
                  <p className="w-5/6 font-sans text-xs ml-2.5 text-yellow-900 font-thin"  >{currentModal.titleDescription}</p>
-               <TextBox placeholder="add comments" buttonName="send" />      
-              <Comments comments={ currentModal.comments } />
+                  <TextBox placeholder="add comments" buttonName="send" />
+                   <ReactionsButtons />
+                   <Comments comments={ currentModal.comments } />
              
                 </div>
     </div>
          </div>
   )
 }
+
+type propsType = {
+  posted: imagesPostedByTheOwner,
+  setOpenModal: React.Dispatch<React.SetStateAction<boolean>>
+}
+
+
+  // Open modal  on profilepage/photos onclick
+
+export const PhotoModal = ({ posted, setOpenModal }: propsType) => {
+    
+  
+ 
+  return (
+    <>
+     <div className=" bg-[#000000ad] fixed top-0 bottom-0 flex items-center justify-center left-0 right-0  z-50">
+          <div className="relative bg-white h-[450px] w-[700px] flex" key={posted.image}  >
+                <IoMdClose onClick={()=>setOpenModal(false)} className="absolute top-0 right-0" fontSize={30} />
+              <img src={ posted.image } className='rounded-lg w-1/2 h-full object-center object-cover' />                      
+            <div className="w-full flex flex-col gap-4">
+               <IoIosClose fontSize={23} className='text-lg absolute top-0 right-0 hover:text-3xl' onClick={()=>setOpenModal(false)} />
+                   <div className="flex justify-start self-baseline ml-2.5 gap-2.5">
+            { posted.image ?
+              <img className='w-6 h-6 rounded-full object-cover object-center ' src={profileImg} /> : <ProfileImage profileName="{ posted.owner }" /> }
+            <div> 
+               <p className='text-sm font-thin text-black font-sans leading-tight'>
+                You
+               </p>
+               <p className='text-xs font-thin text-slate-500 font-sans leading-tight'>1 hour ago</p>
+            </div>
+              </div>
+                 <p className="w-5/6 font-sans text-xs ml-2.5 text-black font-normal"  >{posted.titleDescription}</p>
+            <TextBox placeholder="add comments" buttonName="send" /> 
+           
+            {/* interacted reactions  by users */ }
+                      <ReactionsButtons />
+             {/* Commentes  by users */ }
+               <Comments comments={ posted.comments } />
+             
+            </div>        
+        </div>
+              
+      </div>
+    </>
+  )
+}
+
+
+
+
 
  
--- src/components/PostedCard.tsx
+++ src/components/PostedCard.tsx
@@ -9,9 +9,9 @@ import { BiLike } from 'react-icons/bi'
 import Reactions, { ReactionsButtons } from './Reactions'
 
 
-type propsType = { items: postedContentsDataType, index: number, setCurrentIndex:  React.Dispatch<React.SetStateAction<number>> }
+type propsType = { postedItems: postedContentsDataType, index: number, setCurrentIndex:  React.Dispatch<React.SetStateAction<number>> }
 
-const PostedCard = ({items,index,setCurrentIndex}:propsType) => {
+const PostedCard = ({postedItems,index,setCurrentIndex}:propsType) => {
   
   const [ openLikeModal, setOpenLikeModal ] = useState<boolean>(false)
   const [slideImageNums,setSlideImageNums] =useState<number>(0)
@@ -21,36 +21,47 @@ const PostedCard = ({items,index,setCurrentIndex}:propsType) => {
   return (
      <div className="bg-white flex justify-center flex-col gap-4 items-center border rounded-[5px] p-1.5 border-[#d9d9d9]" >
           <div className="flex justify-start self-baseline ml-2.5 gap-2.5">
-        { items.image ? <img className='w-6 h-6 rounded-full object-cover object-center' src={ items.image[ 0 ] } /> :
-          <ProfileImage profileName={ items.owner } /> }
+        { postedItems.image ? <img className='w-6 h-6 rounded-full object-cover object-center' src={ postedItems.image[ 0 ] } /> :
+          <ProfileImage profileName={ postedItems.owner } /> }
             <div> 
                <p className='text-sm font-bold text-slate-700 font-sans leading-tight'>
-                {items.owner}
+                {postedItems.owner}
                </p>
                <p className='text-xs font-thin text-slate-500 font-sans leading-tight'>1 hour ago</p>
              </div>
           </div>
             <div className="relative w-11/12 flex flex-col gap-6" onClick={()=>setCurrentIndex(index)}>
-                 <p className="w-5/6 font-sans text-xs ml-2.5 text-black font-normal"  >{items.titleDescription}</p>
-                 <div className="flex m-auto w-full rounded-sm gap-0.5 overflow-x-scroll ">
-                      {
-                        items.image.map((img,i)=>(
-                          <img className='h-64 w-full object-cover self-center items-end m-auto object-top' key={i} src={img}  />
-                        ))
-                      }
+                 <p className="w-5/6 font-sans text-xs ml-2.5 text-black font-normal"  >{postedItems.titleDescription}</p>
+                 <div className="flex w-full rounded-sm gap-0.5 overflow-x-scroll ">
+                      <img className={`h-64  ${postedItems.image.length > 1 ?"w-10/12" :"w-full"} object-cover self-center items-end m-auto object-top`}  src={postedItems.image[0]}  />
+               { postedItems.image.length > 1 &&
+                       <div className="relative flex w-1/4 h-[255px] flex-col rounded-md  gap-1 overflow-hidden ">
+                                  {
+                                    postedItems.image.slice(0,3).map((img,i)=>(
+                                      <img className='w-full h-auto object-cover self-center items-end object-top' key={i} src={img}  />
+                                      ))
+                                  }
+                                    <span className='absolute top-0 b-0 h- !opacity-75 rounded-md w-full h-full bg-black text-white flex justify-center items-center hover:opacity-55 cursor-pointer'>
+                                           { postedItems.image.length} +
+                                    </span>
+                      </div>}
            </div>
          
       </div>
      
       <div className='relative flex flex-col w-10/12  gap-4'>
-        { openLikeModal ? <ReactionsButtons />: null}
+        { openLikeModal ?
+          <div className="absolute z-50 -top-4 left-2">
+             <ReactionsButtons />
+          </div>
+          : null }
          <div className="relative flex justify-between" >
           <div className="bg-white flex items-center rounded-xl px-1.5 py-0.5">
            <Reactions />
                 </div>
                 <span className=" flex justify-between gap-2">
-                    <label className='flex items-center  text-xs gap-1 text-slate-400 font-mono' ><span className='font-mono text-xs '>{items.comments.length} </span>comments </label>
-                    <label className='flex items-center  text-xs gap-1 text-slate-400 font-mono '><span className='font-mono text-xs  '>{items.share}</span>share </label>
+                    <label className='flex items-center  text-xs gap-1 text-slate-400 font-mono' ><span className='font-mono text-xs '>{postedItems.comments.length} </span>comments </label>
+                    <label className='flex items-center  text-xs gap-1 text-slate-400 font-mono '><span className='font-mono text-xs  '>{postedItems.share}</span>share </label>
                 </span>
           </div>
             <hr className='h-0.5 w-full'/>
