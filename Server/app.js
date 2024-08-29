6ecc16a91a14999892888e814ef1b8e40cedb199
Abel Zewdu <abelrula716@gmail.com>
2024-08-29 02:17:05 -0900

    Implement backend node-js starter

--- Server/app.js
+++ Server/app.js
@@ -1,1 +1,13 @@
-// server Logic
+ const express = require( "express" )
+const PORT=3000
+
+const app = express()
+app.get( "/",( req, res ) =>
+{
+   res.send("whooooop")  
+})
+app.listen( PORT, () =>
+{
+    console.log(`running on port ${PORT}`);
+    
+})
